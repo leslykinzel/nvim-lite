94c17eee3f077775c2c265a8af3e69a5f4b202ac
Lesly Kinzel <lesly.helios@proton.me>
2025-10-14 13:05:13 +1100

    Whitespace fixes

--- lua/options.lua
+++ lua/options.lua
@@ -12,16 +12,16 @@ vim.opt.list        = true
 vim.opt.listchars   = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Interface
-vim.opt.updatetime    = 250
-vim.opt.number        = false
-vim.opt.splitright    = true
-vim.opt.splitbelow    = true
-vim.opt.showmode      = false
-vim.opt.mouse         = "a"
-vim.opt.signcolumn    = "yes"
-vim.opt.scrolloff     = 8
-vim.opt.cmdheight     = 1
-vim.opt.cmdwinheight  = 10
+vim.opt.updatetime   = 250
+vim.opt.number       = false
+vim.opt.splitright   = true
+vim.opt.splitbelow   = true
+vim.opt.showmode     = false
+vim.opt.mouse        = "a"
+vim.opt.signcolumn   = "yes"
+vim.opt.scrolloff    = 8
+vim.opt.cmdheight    = 1
+vim.opt.cmdwinheight = 10
 vim.cmd("colorscheme default")
 
 -- Search
